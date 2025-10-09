67e27b3467be189195d32a4c343951a4ce696dba
ivyevellinti <ivy.evellin01@gmail.com>
2025-10-09 12:04:16 -0300

    Início da craição da página de login

---
+++ View/pagina-login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ClassAi | Login</title>
+    <link rel="stylesheet" href="../Templates/css/pagina-login.css">
+</head>
+<body>
+    <div class="container">
+    <header>
+        <a href="">
+            <figure>
+                <img src="../Images/Ícones do header/Logo ClassAi branca.png" alt="Logo ClassAi">
+            </figure>
+        </a>
+    </header>
+
+    <main>
+        <div class="conteudo_principal">
+        <form action="" style="display: block">
+            <h1 style="color: white">Bem vindo de volta!</h1>
+            <h2 style="color: white">O que vamos aprender hoje?</h2>
+            
+            <div class="inputs">
+            <input type="text" name="userEmail" class="userEmail" placeholder="E-mail">
+            <input type="text" name="userPassword" class="userPassword" placeholder="Senha">
+            <p style="color: #C37BFF">Esqueceu a senha?</p>
+            </div>
+
+            <div class="buttons">
+                <button> <img class="logoGoogle" src="../Images/Login/Logo Google.png" alt="Logo Google"> Continuar com o Google</button>
+                <button> <img class="logoApple" src="../Images/Login/Logo Apple.png" alt="Logo Apple"> Continuar com a Apple</button>
+                <button>Entrar</button>
+            </div>
+            <p class="nao_tem_conta">Não tem conta? <a class="cadastre_se" href="">Cadastre-se</a></p>
+        </form>
+
+        <div class="lazzo">
+            <figure>
+                <img src="../Images/Login/LazzoTexto.png" alt="Lazzo">
+            </figure>
+        </div>
+        </div>
+    </main>
+
+    </div>  
+</body>
+</html>
