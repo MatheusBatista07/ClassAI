e53c0bb63955568765f5d991ee3a05fa294b1934
MatheusBatista07 <bomfimmatheus07@gmail.com>
2025-10-14 11:47:07 -0300

    feat: Finish

---
+++ View/PaginaCertificado.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ClassAI- Certificado</title>
+    <link rel="stylesheet" href="../Templates/css/Certificado.css">
+</head>
+<body>
+    <header>
+        <button class="voltar">
+            <img src="../Images/Certificado/Seta_retornar.png" alt="Seta branca de retornar">
+        </button>
+
+        <div class="botoes">
+            <button class="btn">
+                <img src="../Images/Certificado/dowload.png" alt="Ícone de dowload">
+            </button>
+            <button class="btn">
+                <img src="../Images/Certificado/Compartilhar.png" alt="Ícone de compartilhamento">
+            </button>
+        </div>
+    </header>
+
+    <main>
+        <div class="tudo">
+            <img src="../Images/Certificado/logo_roxa.png" alt="Logo do ClassAI roxa">
+
+            <h1 class="cert">Certificado</h1>
+            <p class="txt1">de conclusão de curso</p>
+
+            <h1 class="nome">Jeferson Brito Souza</h1>
+
+            <p class="txt2">
+                Certificamos que Jeferson Souza concluiu com êxito o curso "Inteligência Artificial 
+                para Professores e Educadores", demonstrando compromisso com a inovação e o aprimoramento 
+                das práticas pedagógicas por meio do uso de tecnologias emergentes.
+            </p>
+
+            <div class="ass">
+                <h2>Jõao Silva Gomes</h2>
+            </div>
+            <p class="prof">Professor Responsável</p>
+
+            <p class="ano">2025</p>
+        </div>
+    </main>
+</body>
+</html>
