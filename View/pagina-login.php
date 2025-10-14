cd17b8b5f56f49675de096081aa7a89615ef700d
ivyevellinti <ivy.evellin01@gmail.com>
2025-10-14 10:59:37 -0300

    finalização do css da página de login | pendencia no javascrip e hrefs

--- View/pagina-login.php
+++ View/pagina-login.php
@@ -25,13 +25,13 @@
             <div class="inputs">
             <input type="text" name="userEmail" class="userEmail" placeholder="E-mail">
             <input type="text" name="userPassword" class="userPassword" placeholder="Senha">
-            <p style="color: #C37BFF">Esqueceu a senha?</p>
+            <p class="esqueceuSenha" style="color: #C37BFF">Esqueceu a senha?</p>
             </div>
 
             <div class="buttons">
-                <button> <img class="logoGoogle" src="../Images/Login/Logo Google.png" alt="Logo Google"> Continuar com o Google</button>
-                <button> <img class="logoApple" src="../Images/Login/Logo Apple.png" alt="Logo Apple"> Continuar com a Apple</button>
-                <button>Entrar</button>
+                <a href="https://accounts.google.com/v3/signin/accountchooser?dsh=S1930013800%3A1760449800043570&elo=1&ifkv=AfYwgwXw6nL7aYaFXBT9ctMlYrS5YZZXUvRLyOiyCQpWJtM7WKLA9ww8TZizvliCwOEeET5L_2_m&flowName=GlifWebSignIn&flowEntry=ServiceLogin" class="continuarCom"> <img class="logo" src="../Images/Login/Logo Google.png" alt="Logo Google"> Continuar com o Google</a>
+                <a href="" class="continuarCom"> <img class="logo" src="../Images/Login/Logo Apple.png" alt="Logo Apple"> Continuar com a Apple</a>
+                <button class="entrar">Entrar</button>
             </div>
             <p class="nao_tem_conta" style="color: #C37BFF">Não tem conta? <a class="cadastre_se" href="" style="color: #C37BFF">Cadastre-se</a></p>
         </form>
