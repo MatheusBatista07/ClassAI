27ceea2f10fe20007d73b9543a95f655359307fc
MatheusBatista07 <bomfimmatheus07@gmail.com>
2025-10-14 12:28:30 -0300

    feat: Iniciando

---
+++ View/PaginaPrincipalCursos.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ClassAI - Cursos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="../Templates/css/Cursos.css">
+
+</head>
+
+<body>
+    <div class="sidebar">
+        <img src="../Images/Ícones do header/Logo ClassAI branca.png" alt="Imagem logo ClassAII" class="img-logo">
+
+        <ul class="nav-menu">
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="bi bi-house-door"></i>
+                    Principal
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="bi bi-chat"></i>
+                    Chat
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="bi bi-book"></i>
+                    Cursos
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link active">
+                    <i class="bi bi-award"></i>
+                    Certificados
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="bi bi-people"></i>
+                    Amigos
+                </a>
+            </li>
+        </ul>
+
+        <div class="nav-divider"></div>
+
+        <ul class="nav-menu">
+
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="bi bi-question-circle"></i>
+                    Ajuda e FAQ
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    Modo escuro
+                    <i class="bi bi-moon-stars ms-auto"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="main-content">
+        <!-- Header -->
+        <div class="header">
+            <div></div>
+            <div class="header-icons">
+                <div class="header-icon">
+                    <img src="../Images/Ícones do header/lazzo.png" alt="Imagem lazzo" class="lazzo_img">
+                </div>
+                <div class="header-icon">
+                    <i class="bi bi-bell"></i>
+                </div>
+
+                <div class="user-profile">
+                    <img src="https://via.placeholder.com/40" alt="Avatar do Usuário" class="user-avatar">
+                    <img src="../Images/Ícones do header/setinha perfil.png" alt="Seta" class="arrow-icon">
+                </div>
+
+
+            </div>
+        </div>
+    <div class="cima">
+        <h1>Página de Cursos</h1>
+        <div class="pesquisa">
+            <input type="text" placeholder="Pesquisar">
+            <button class="pesq">
+                <img src="../Images/Ícones do header/Lupa_branca.png" alt="Lupa branca">
+            </button>
+        </div>
+
+        <div class="filtro">
+            <p>Filtrar</p>
+        </div>
+    </div>
+
+</body>
+</html>
