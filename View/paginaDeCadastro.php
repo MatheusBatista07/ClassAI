69110465147eaf5e0f45fba6873fed916a3e8128
Chaves777 <lc102434@gmail.com>
2025-10-21 12:03:13 -0300

    feat: html e css pagina de cadastro completos

--- View/paginaDeCadastro.php
+++ View/paginaDeCadastro.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ClassAi | Login</title>
+    <title>ClassAi | Cadastro</title>
     <link rel="stylesheet" href="../Templates/css/paginaDeCadastro.css">
 </head>
+
 <body>
-    
+
     <header>
         <a href="">
             <figure>
@@ -15,35 +17,47 @@
             </figure>
         </a>
     </header>
-<div class="container">
+    <div class="container">
         <main>
-        <div class="conteudo_principal">
-        <form action="" class="formulario" style="display: block; justify-content: center; align-items: center"">
-            <h1 style="color: white">Bem vindo de volta!</h1>
-            <h2 style="color: white">O que vamos aprender hoje?</h2>
-            
-            <div class="inputs">
-            <input type="text" name="userEmail" class="userEmail" placeholder="E-mail">
-            <input type="text" name="userPassword" class="userPassword" placeholder="Senha">
-            <p class="esqueceuSenha" style="color: #C37BFF">Esqueceu a senha?</p>
+            <div class="conteudo_principal">
+                <form action="" class="formulario" style="display: block; justify-content: center; align-items: center"">
+            <h1 style=" color: white">Que bom ver você!</h1>
+                    <h2 style="color: white">O seu futuro começa aqui!</h2>
+
+                    <div class="inputs">
+                        <input type="text" name="userEmail" class="userEmail" placeholder="E-mail">
+                        <input type="text" name="userPassword" class="userPassword" placeholder="Senha">
+                        <input type="text" name="userPasswordConfirm" class="userPassword" placeholder="Confirmar senha">
+                        <div class="form-check" style="color: #C37BFF;">
+                            <input class="form-check-input" type="checkbox" id="termosCheck" required>
+                            <label class="form-check-label" for="termosCheck">
+                                Li e estou de acordo com o
+                                <a href="#" style="color: #C37BFF; text-decoration: underline;">Termo de Uso</a>
+                                e
+                                <a href="#" style="color: #C37BFF; text-decoration: underline;">Política de
+                                    Privacidade</a>
+                            </label>
+                        </div>
+                    </div>
+
+
+
+                    <div class="buttons">
+                        <button class="entrar">Cadastrar</button>
+                    </div>
+                    <p class="nao_tem_conta" style="color: #C37BFF">Já tem conta? <a class="cadastre_se" href=""
+                            style="color: #C37BFF">Entrar</a></p>
+                </form>
+
+                <div class="lazzo">
+                    <figure>
+                        <img class="lazzoImg" src="../Images/Página de Login e Cadastro/LazzoCadastro.png" alt="Lazzo">
+                    </figure>
+                </div>
             </div>
+        </main>
 
-            <div class="buttons">
-                <a href="https://accounts.google.com/v3/signin/accountchooser?dsh=S1930013800%3A1760449800043570&elo=1&ifkv=AfYwgwXw6nL7aYaFXBT9ctMlYrS5YZZXUvRLyOiyCQpWJtM7WKLA9ww8TZizvliCwOEeET5L_2_m&flowName=GlifWebSignIn&flowEntry=ServiceLogin" class="continuarCom"> <img class="logo" src="../Images/Login/Logo Google.png" alt="Logo Google"> Continuar com o Google</a>
-                <a href="" class="continuarCom"> <img class="logo" src="../Images/Login/Logo Apple.png" alt="Logo Apple"> Continuar com a Apple</a>
-                <button class="entrar">Entrar</button>
-            </div>
-            <p class="nao_tem_conta" style="color: #C37BFF">Não tem conta? <a class="cadastre_se" href="" style="color: #C37BFF">Cadastre-se</a></p>
-        </form>
-
-        <div class="lazzo">
-            <figure>
-                <img class="lazzoImg" src="../Images/Login/LazzoTexto.png" alt="Lazzo">
-            </figure>
-        </div>
-        </div>
-    </main>
-
-    </div>  
+    </div>
 </body>
+
 </html>
