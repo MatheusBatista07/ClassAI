2fa1c8b9156add6b1858f6cb0b94f599e7270b3a
Spaceza <raphaelssposito@gmail.com>
2025-11-05 11:45:00 -0300

    fix: limpeza dos arquivos

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ivy/classai',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1ee429fc47400b8ab8b36ee8e366845e65b05c83',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ivy/classai' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1ee429fc47400b8ab8b36ee8e366845e65b05c83',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
