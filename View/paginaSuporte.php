704c32903368de8e0c13494bd77a2f9758690bc9
Chaves777 <lc102434@gmail.com>
2025-11-06 12:25:48 -0300

    feat: pagina de suporte

---
+++ View/paginaSuporte.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ClassAi | Suporte</title>
+    <link rel="stylesheet" href="../Templates/css/paginaSuporte.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body>
+       <header>
+        <a href="index.php">
+            <figure>
+                <img src="../Images/Ícones do header/Logo ClassAi branca.png" alt="Logo ClassAi">
+            </figure>
+            
+        </a>
+    </header>
+
+    <div class="container">
+        <main>
+            <div class="conteudo_principal">
+                <form action="paginaSuporte.php" method="POST" class="formulario">
+                    <img src="../Images/Página suporte/girl.png" alt="imagem ilustrativa">  
+                    
+            </div>
+        </main>
+    </div>
+    <!-- O JS de validação foi removido/limpo, então ele não vai mais interferir -->
+    <script src="../Templates/js/paginaSuporte.js"></script> 
+</body>
+</html>
