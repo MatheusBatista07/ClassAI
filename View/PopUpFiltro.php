64fec607341386f6e53674bf4ab4455790c88c1d
Spaceza <raphaelssposito@gmail.com>
2025-11-10 11:49:23 -0300

    feature: Estutura completa PopUpPerfil

---
+++ View/PopUpFiltro.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Popup de Filtro com Expansão</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="../Templates/css/PopUpFiltro.css">
+</head>
+<body>
+
+    <div class="filter-popup-outer">
+        <div class="filter-popup-inner">
+            <ul class="filter-menu">
+                <li>
+                    <a href="#" class="expanding-button">
+                        <i class="bi bi-plus-square-fill"></i>
+                        <span>Saúde</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="expanding-button">
+                        <i class="bi bi-gear-wide-connected"></i>
+                        <span>Engenharia</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="expanding-button">
+                        <i class="bi bi-tools"></i>
+                        <span>Mecânica</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="expanding-button">
+                        <i class="bi bi-briefcase-fill"></i>
+                        <span>Direito</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="expanding-button">
+                        <i class="bi bi-laptop-fill"></i>
+                        <span>Informática</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</body>
+</html>
